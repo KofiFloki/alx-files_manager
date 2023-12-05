712859fc41be2c9783e13740ec147d8ef32a2ca8
KofiFloki <Jeremiah.marteey@yahoo.com>
2023-12-04 20:20:01 -0800

    First API

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = require('./routes/index');
+
+const app = express();
+const port = process.env.PORT || '5000';
+
+app.use('/', router);
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
